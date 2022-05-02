e2a590eaee48979e880702efd51065295c05390a
Warren Snipes <contact@warrensnipes.dev>
2022-05-01 20:17:06 -0400

    Added else case to compose script

--- compose.sh
+++ compose.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ] || [ "$1" = "prod" ]; then
-  rover supergraph compose --config "./supergraph-$1.yaml" > schema.graphql
+  rover supergraph compose --config "./supergraph-$1.yaml" >schema.graphql
+else
+  printf "You must supply dev or prod as arguments to the script\n"
 fi
